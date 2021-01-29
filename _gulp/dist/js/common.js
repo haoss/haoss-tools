6c0c538f011c30e65d762041bf42fbf8d1789863
Dmitriy Khristoev <haossut@gmail.com>
2021-01-29 11:54:10 +0200

    updates files

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -3,60 +3,6 @@
 // Document ready
 $(document).on('ready', function(){
 
-  // E-mail Ajax Send
-  // Documentation & Example: https://github.com/agragregra/uniMail
-  $("form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function() {
-      alert("Thank you!");
-      setTimeout(function() {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
-
-  // Magnific popup gallery
-  $('.gallery').each(function() {
-    $(this).magnificPopup({
-      delegate: '.gallery-item',
-      type: 'image',
-      gallery:{
-        enabled:true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      }
-    });
-  });
-
-  // Magnific popup one image
-  $('.image-popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    mainClass: 'mfp-img-mobile',
-    image: {
-      verticalFit: true
-    }
-  });
-
   // Magnific popup video
   $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
     disableOn: 700,
@@ -72,6 +18,48 @@ $(document).on('ready', function(){
     midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
   });
 
+  $("form").each(function(){
+    $(this).validate();
+  });
+
+  $('.svg-inline').each(function() {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+    jQuery.get(imgURL, function(data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+      // Add replaced image's ID to the new SVG
+      if (typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if (typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass + ' replaced-svg');
+      }
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+    }, 'xml');
+  });
+
+  $('[data-fancybox]').fancybox({
+    protect: true,
+    backFocus: false
+  });
+
+  $("a[href*='#'].anchor").mPageScroll2id({
+    offset: '.header'
+  });
+
+  phoneMask();
+  mobileNav();
+  inputFocus();
+  headerScroll();
+  oneCarousel();
+
   // Chrome Smooth Scroll
   try {
     $.browserSelector();
@@ -86,12 +74,23 @@ $(document).on('ready', function(){
   simpleForm('form.form-callback');
 });
 
-$(window).on('load', function() {
-  $(".loader").delay(400).fadeOut("slow");
+$(window).on('load', function() { });
+$(window).on('scroll', function() {
+  headerScroll();
 });
+$(window).on('resize', function() {
+  var width = $(window).width();
+  /*var btn = $('.btn-mobile');
+  var body = $('body');
+  var nav = $('.mobile-nav');*/
 
-$(window).on('scroll', function() { });
-$(window).on('resize', function() { });
+  if (width >= 960) {
+    /*btn.removeClass('is-active');
+    body.removeClass('is-fixed');
+    nav.removeClass('is-active');
+    $('.j-footer-nav').removeClass('is-active');*/
+  }
+});
 
 /*
 version 2015-09-23 14:30 GMT +2
@@ -155,4 +154,131 @@ function simpleForm(form, callback) {
 
     return false;
   });
+}
+
+function phoneMask() {
+  var phone = $('.j-phone-mask');
+  phone.each(function () {
+    $(this).mask("+7 (999) 999-99-99");
+  });
+}
+
+jQuery.extend(jQuery.validator.messages, {
+  required: "Обязательное поле",
+  remote: "Please fix this field.",
+  email: "Введите правильный e-mail.",
+  url: "Please enter a valid URL.",
+  date: "Please enter a valid date.",
+  dateISO: "Please enter a valid date (ISO).",
+  number: "Please enter a valid number.",
+  digits: "Please enter only digits.",
+  creditcard: "Please enter a valid credit card number.",
+  equalTo: "Пароли не совпадают.",
+
+  accept: "Please enter a value with a valid extension.",
+  maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+  minlength: jQuery.validator.format("Please enter at least {0} characters."),
+  rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+  range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+  max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+  min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+});
+
+function mobileNav() {
+  /*var btn = $('.btn-mobile');
+  var body = $('body');
+  var nav = $('.mobile-nav');
+  var navWrapper = $('.mobile-nav__wrapper');
+
+  btn.on('click', function(){
+    var _this = $(this);
+    if (_this.hasClass('is-active')) {
+      btn.removeClass('is-active');
+      body.removeClass('is-fixed');
+      nav.removeClass('is-active');
+    } else {
+      btn.addClass('is-active');
+      body.addClass('is-fixed');
+      nav.addClass('is-active');
+    }
+  });
+
+  nav.on('click', function(){
+    btn.removeClass('is-active');
+    body.removeClass('is-fixed');
+    nav.removeClass('is-active');
+  });
+
+  navWrapper.on('click', function(e){
+    e.stopPropagation();
+  })*/
+}
+
+function inputFocus(){
+  var jinput = $(".css-input");
+
+  jinput.each(function(){
+    var _this = $(this);
+    var val = _this.val();
+    var field = _this.parents('.j-field-text');
+
+    if (val.length > 0 && _this.is('input') || val.length > 0 && _this.is('textarea')) {
+      field.addClass("active-full");
+    } else {
+      field.removeClass("active-full");
+    }
+
+    // input on focus
+    _this.focus(function () {
+      field.addClass("active");
+    }).blur(function () {
+      field.removeClass("active");
+    })
+
+    _this.on('change', function () {
+      var val = _this.val();
+
+      if (val == '') {
+        field.removeClass("active-full");
+      } else {
+        field.addClass("active-full");
+      }
+    });
+  })
+}
+
+function headerScroll() {
+  var header = $('.header');
+  var width = $(window).width();
+
+  if ($(window).scrollTop() > header.height()) {
+    header.addClass('is-scroll');
+  } else {
+    header.removeClass('is-scroll');
+  }
+}
+
+function oneCarousel() {
+  var width = $(window).width();
+
+  var $slickElementPagination = $('.one-apartment__carousel-pagination');
+  var $slickElement = $('.j-one-apartment');
+
+  if (width <= 767) {
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+      //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+      var i = (currentSlide ? currentSlide : 0) + 1;
+      $slickElementPagination.text(i + ' / ' + slick.slideCount);
+    });  
+    $slickElement.not('.slick-initialized').slick({
+      mobileFirst: true,
+      arrows: false,
+      infinite: true,
+      dots: false
+    });
+  } else {
+    if($slickElement.hasClass('slick-initialized')){
+      $slickElement.slick('unslick');
+    }
+  }
 }
